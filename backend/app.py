0e32839f9bbd99ccb1ee6ad9a3835af1c053381a
Patrick Gouto <goutopatrick6@gmail.com>
2026-02-11 02:38:55 +0100

    inserimento funzioni

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,31 @@
-print("ciao mondo")
+#LEARNING TRACKER
+
+
+
+#1) Utente deve inserire il dato!
+#2) Dato deve essere salvato in un file JSON
+#3) Utente deve poter visualizzare i dati salvati
+#4) Utente deve poter modificare i dati salvati
+
+
+def aggiungi_materia():
+    nome_materia = input("Inserisci il nome della materia: ")
+    livello_attuale = input("Inserisci il livello attuale (base, intermedio, avanzato): ")
+    tempo_studio = input("Inserisci il tempo di studio dedicato (in ore): ")
+    # Salva i dati in un file JSON
+    #.......
+
+
+def visualizza_materie():
+    # Leggi i dati dal file JSON e visualizzali
+    #.......
+
+
+
+def organizza_studio():
+    # Organizza il piano di studio in base ai dati salvati
+    #.......
+
+def progresso():
+    # Mostra il progresso dell'utente in base ai dati salvati
+    #.......
