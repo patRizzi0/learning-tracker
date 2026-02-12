7d8b70c5175bf5c46651d114a97606fab3690ae3
Patrick <tuaemail@example.com>
2026-02-12 01:18:21 +0100

    Implement JSON data handling for subjects and enhance study organization logic

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 #LEARNING TRACKER
 
-
+import json
 
 #1) Utente deve inserire il dato!
 #2) Dato deve essere salvato in un file JSON
@@ -12,20 +12,51 @@ def aggiungi_materia():
     nome_materia = input("Inserisci il nome della materia: ")
     livello_attuale = input("Inserisci il livello attuale (base, intermedio, avanzato): ")
     tempo_studio = input("Inserisci il tempo di studio dedicato (in ore): ")
-    # Salva i dati in un file JSON
-    #.......
+    dump_materia(nome_materia, livello_attuale, tempo_studio)
+
+def dump_materia(nome_materia, livello_attuale, tempo_studio):
+    materia = {
+        "nome": nome_materia,
+        "livello": livello_attuale,
+        "tempo_studio": tempo_studio
+    }
+    
+    # Salva dati aggiornati
+    with open('backend/data.json', 'w') as file:
+        json.dump(materia, file, indent=4)
 
 
+#aggiungi_materia()
+
+
+#Visualizza i dati salvati
 def visualizza_materie():
-    # Leggi i dati dal file JSON e visualizzali
-    #.......
+    with open("backend/data.json", "r") as file:
+        dati = json.load(file)
+        
+    for materia in dati:
+        print(materia)
 
+visualizza_materie()
 
 
 def organizza_studio():
-    # Organizza il piano di studio in base ai dati salvati
-    #.......
+    with open("backend/data.json", "r") as file:
+        dati = json.load(file)
+
+        for materia in dati:
+            livello = materia["livello"]
+
+            print(livello)
+            if livello == "base":
+                print("Organizza sessioni di studio più brevi e frequenti.")
+            elif livello == "intermedio":
+                print("Aumenta gradualmente la durata delle sessioni di studio.")
+            elif livello == "avanzato":
+                print("Focalizzati su progetti pratici e approfondimenti.")
+
+
+organizza_studio()
 
 def progresso():
-    # Mostra il progresso dell'utente in base ai dati salvati
-    #.......
+    
